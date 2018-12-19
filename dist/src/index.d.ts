08979b52a4a50a757dcd35d44a255e3bec475a84
Волков Олег Евгеньевич <volkov.o.e@yandex.ru>
2018-12-19 15:23:04 +0300

    6

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,3 +1,3 @@
 export * from "./models/index";
 export * from "./services/index";
-export * from "../../goodwill-library.module";
+export {GoodWillLibraryModule} from "../../goodwill-library.module";
